6e3e0c644e26cdd3ff9264f02b942cf575b99614
jake <bigjake2s@gmail.com>
2022-03-23 09:58:43 -0400

    Adding a bunch of structs to flesh out some thoughts

--- pkg/document/document.go
+++ pkg/document/document.go
@@ -1,6 +1,43 @@
 package document
 
+type Option struct {
+	Value    string
+	Label    string
+	Selected bool
+}
+
+type Type int
+
+type Field struct {
+	Id      int64
+	Name    string
+	Label   string
+	Type    Type
+	Options []Option
+	Fields  []Field
+}
+
+type Class struct {
+	Id     int64
+	Slug   string
+	Fields []Field
+}
+
 type Document struct {
 	Id     int64
-	Fields map[string]interface{}
+	Slug   string
+	Class  Class
+	Values map[string]interface{}
 }
+
+const (
+	Text Type = iota
+	TextArea
+	Number
+	Date
+	Time
+	Email
+	Select
+	MultiSelect
+	Section
+)
