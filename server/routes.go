f039e134a0fd83a5acf8dc2f651ff15aa68837e7
jake <bigjake2s@gmail.com>
2022-04-04 16:49:10 -0400

    Route planning

--- server/routes.go
+++ server/routes.go
@@ -7,20 +7,29 @@ import (
 func (s *Server) Routes() *gin.Engine {
 	router := s.router
 
+	// router.GET("/assets/:filename", s.HandleAsset())
+	// router.GET("/forms/:id", s.HandleForm())
+	// router.POST("/forms/:id", s.HandleForm())
+
 	admin := router.Group("/admin")
 	{
 		classes := admin.Group("/classes")
 		{
 			classes.GET("/new", s.HandleClassBuilder())
 			classes.POST("/new", s.HandleClassBuilder())
-			class := classes.Group("/:slug")
+			class := classes.Group("/:id")
 			{
+				// class.Get("/", s.HandleClassIndex())
 				class.GET("/edit", s.HandleClassBuilder())
 				class.POST("/edit", s.HandleClassBuilder())
 				class.GET("/fields", s.HandleClassFieldBuilder())
 				class.POST("/fields", s.HandleClassFieldBuilder())
 			}
 		}
+		// forms := admin.Group("/forms")
+		// 	forms.GET("/new", s.HandleFormBuilder())
+		// 	forms.POST("/new", s.HandleFormBuilder())
+		// }
 	}
 
 	return router
