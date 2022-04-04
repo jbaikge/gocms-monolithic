79a5aac6c9041981979fc680d14fd250d4ef2c73
jake <bigjake2s@gmail.com>
2022-04-04 17:20:05 -0400

    Adding form marshalling tags to class members

--- class.go
+++ class.go
@@ -8,8 +8,8 @@ import (
 
 type Class struct {
 	Id      primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name    string             `json:"name"`
-	Slug    string             `json:"slug"`
+	Name    string             `form:"name" json:"name"`
+	Slug    string             `form:"slug" json:"slug"`
 	Created time.Time          `json:"created"`
 	Updated time.Time          `json:"updated"`
 	Fields  []Field            `json:"fields"`
