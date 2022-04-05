acb82c2fb74c22ec96d376e11bfec5ef4e9b9376
jake <bigjake2s@gmail.com>
2022-04-05 10:50:02 -0400

    Adding classService parameter to server.New()

--- server/server.go
+++ server/server.go
@@ -3,18 +3,28 @@ package server
 import (
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
+	"github.com/jbaikge/gocms"
 )
 
 type Server struct {
+	classService gocms.ClassService
+	// documentService gocms.DocumentService
 	renderer     multitemplate.Renderer
 	router       *gin.Engine
 	templatePath string
 }
 
-func New(templatePath string, router *gin.Engine) *Server {
+func New(
+	templatePath string,
+	router *gin.Engine,
+	classService gocms.ClassService,
+	// documentService gocms.DocumentService,
+) *Server {
 	renderer := multitemplate.NewRenderer()
 	router.HTMLRender = renderer
 	return &Server{
+		classService: classService,
+		// documentService: documentService,
 		renderer:     renderer,
 		router:       router,
 		templatePath: templatePath,
