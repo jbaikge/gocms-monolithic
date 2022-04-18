d0d84514e8c395df3172cef2dede4439998b576e
jake <bigjake2s@gmail.com>
2022-04-18 11:37:38 -0400

    Adding memory repository and repository suite tests

---
+++ repository/memory.go
@@ -0,0 +1,171 @@
+package repository
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/jbaikge/gocms"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type sortClasses []gocms.Class
+
+func (s sortClasses) Len() int           { return len(s) }
+func (s sortClasses) Less(i, j int) bool { return s[i].Name < s[j].Name }
+func (s sortClasses) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+type memoryRepository struct {
+	classes   []gocms.Class
+	documents []gocms.Document
+}
+
+func NewMemory() Repository {
+	return &memoryRepository{
+		classes:   make([]gocms.Class, 0, 128),
+		documents: make([]gocms.Document, 0, 128),
+	}
+}
+
+func (r *memoryRepository) DeleteClass(id primitive.ObjectID) (err error) {
+	for i, class := range r.classes {
+		if class.Id == id {
+			r.classes = append(r.classes[:i], r.classes[i+1:]...)
+			break
+		}
+	}
+	return
+}
+
+func (r *memoryRepository) GetAllClasses() ([]gocms.Class, error) {
+	sort.Sort(sortClasses(r.classes))
+	return r.classes, nil
+}
+
+func (r *memoryRepository) GetClassById(id primitive.ObjectID) (class gocms.Class, err error) {
+	for _, c := range r.classes {
+		if c.Id == id {
+			return c, nil
+		}
+	}
+	err = fmt.Errorf("class not found: %s", id.Hex())
+	return
+}
+
+func (r *memoryRepository) GetClassBySlug(slug string) (class gocms.Class, err error) {
+	for _, c := range r.classes {
+		if c.Slug == slug {
+			return c, nil
+		}
+	}
+	err = fmt.Errorf("class not found: %s", slug)
+	return
+}
+
+func (r *memoryRepository) InsertClass(class *gocms.Class) (err error) {
+	class.Id = primitive.NewObjectID()
+	now := time.Now()
+	class.Created = now
+	class.Updated = now
+	r.classes = append(r.classes, *class)
+	return
+}
+
+func (r *memoryRepository) UpdateClass(class *gocms.Class) (err error) {
+	for i, c := range r.classes {
+		if c.Id == class.Id {
+			class.Updated = time.Now()
+			r.classes[i] = *class
+			return
+		}
+	}
+	return fmt.Errorf("class not found: %s", class.Id.Hex())
+}
+
+func (r *memoryRepository) DeleteDocument(id primitive.ObjectID) (err error) {
+	for i, doc := range r.documents {
+		if doc.Id == id {
+			r.documents = append(r.documents[:i], r.documents[i+1:]...)
+			break
+		}
+	}
+	return
+}
+
+func (r *memoryRepository) GetChildDocumentBySlug(parentId primitive.ObjectID, slug string) (doc gocms.Document, err error) {
+	for _, d := range r.documents {
+		if d.ParentId == parentId && d.Slug == slug {
+			return d, nil
+		}
+	}
+	err = fmt.Errorf("document not found for %s-%s", parentId.Hex(), slug)
+	return
+}
+
+func (r *memoryRepository) GetClassDocumentBySlug(classId primitive.ObjectID, slug string) (doc gocms.Document, err error) {
+	for _, d := range r.documents {
+		if d.ClassId == classId && d.Slug == slug {
+			return d, nil
+		}
+	}
+	err = fmt.Errorf("document not found for %s-%s", classId.Hex(), slug)
+	return
+}
+
+func (r *memoryRepository) GetDocumentList(params gocms.DocumentListParams) (list gocms.DocumentList, err error) {
+	docs := make([]gocms.Document, 0, len(r.documents))
+	for _, doc := range r.documents {
+		if doc.ClassId == params.ClassId {
+			docs = append(docs, doc)
+		}
+	}
+
+	list.Total = int64(len(docs))
+	if list.Total == 0 {
+		return
+	}
+
+	end := params.Offset() + params.Size
+	if end > list.Total {
+		end = list.Total
+	}
+	list.Documents = docs[params.Offset():end]
+
+	return
+}
+
+func (r *memoryRepository) GetDocumentById(id primitive.ObjectID) (doc gocms.Document, err error) {
+	for _, d := range r.documents {
+		if d.Id == id {
+			return d, nil
+		}
+	}
+	err = fmt.Errorf("document not found: %s", id.Hex())
+	return
+}
+
+func (r *memoryRepository) InsertDocument(doc *gocms.Document) (err error) {
+	doc.Id = primitive.NewObjectID()
+	now := time.Now()
+	doc.Created = now
+	doc.Updated = now
+	r.documents = append(r.documents, *doc)
+	return
+}
+
+func (r *memoryRepository) UpdateDocument(doc *gocms.Document) (err error) {
+	for i, d := range r.documents {
+		if d.Id == doc.Id {
+			doc.Updated = time.Now()
+			r.documents[i] = *doc
+			return
+		}
+	}
+	return fmt.Errorf("document not found: %s", doc.Id.Hex())
+}
+
+func (r *memoryRepository) empty() (err error) {
+	r.classes = r.classes[:0]
+	r.documents = r.documents[:0]
+	return
+}
---
+++ repository/repository_test.go
@@ -0,0 +1,261 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/jbaikge/gocms"
+	"github.com/zeebo/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func repositories(t *testing.T) (repos []Repository) {
+	repos = make([]Repository, 0, 2)
+
+	// Mongo Repository
+	dbHost := "localhost:27017"
+	if dbHostEnv := os.Getenv("DB_HOST"); dbHostEnv != "" {
+		dbHost = dbHostEnv
+	}
+
+	dbName := "testing"
+	if dbNameEnv := os.Getenv("DB_NAME"); dbNameEnv != "" {
+		dbName = dbNameEnv
+	}
+
+	opts := options.Client().ApplyURI("mongodb://" + dbHost)
+
+	client, err := mongo.Connect(context.Background(), opts)
+	assert.NoError(t, err)
+
+	mongoDB := client.Database(dbName)
+	// Clean out db before messing with it
+	assert.NoError(t, mongoDB.Drop(context.Background()))
+
+	repos = append(repos, NewMongo(context.Background(), mongoDB))
+
+	// Memory Repository
+	repos = append(repos, NewMemory())
+
+	return
+}
+
+func TestRepository(t *testing.T) {
+	for _, repo := range repositories(t) {
+		t.Run(reflect.TypeOf(repo).Elem().Name(), func(t *testing.T) {
+			t.Run("DeleteClass", func(t *testing.T) {
+				class := gocms.Class{}
+				assert.NoError(t, repo.InsertClass(&class))
+				assert.NoError(t, repo.DeleteClass(class.Id))
+				_, err := repo.GetClassById(class.Id)
+				// Make sure a "no documents in result" error pops out
+				assert.Error(t, err)
+			})
+
+			t.Run("GetAllClasses", func(t *testing.T) {
+				assert.NoError(t, repo.empty())
+				classes := []gocms.Class{
+					{Name: "Event", Slug: "event"},
+					{Name: "Blog", Slug: "blog"},
+					{Name: "News", Slug: "news"},
+				}
+				for _, class := range classes {
+					assert.NoError(t, repo.InsertClass(&class))
+				}
+
+				all, err := repo.GetAllClasses()
+				assert.NoError(t, err)
+				assert.Equal(t, len(classes), len(all))
+
+				expectSlugs := []string{"blog", "event", "news"}
+				for i := range all {
+					assert.Equal(t, all[i].Slug, expectSlugs[i])
+				}
+			})
+
+			t.Run("GetClassById", func(t *testing.T) {
+				class := gocms.Class{
+					Slug: "get_class_by_id",
+				}
+				assert.NoError(t, repo.InsertClass(&class))
+
+				check, err := repo.GetClassById(class.Id)
+				assert.NoError(t, err)
+				assert.Equal(t, class.Slug, check.Slug)
+
+				_, err = repo.GetClassById(primitive.NewObjectID())
+				assert.Error(t, err)
+			})
+
+			t.Run("GetClassBySlug", func(t *testing.T) {
+				class := gocms.Class{
+					Slug: "get_class_by_slug",
+				}
+				assert.NoError(t, repo.InsertClass(&class))
+
+				check, err := repo.GetClassBySlug(class.Slug)
+				assert.NoError(t, err)
+				assert.Equal(t, class.Slug, check.Slug)
+
+				_, err = repo.GetClassBySlug("invalid_slug")
+				assert.Error(t, err)
+			})
+
+			t.Run("InsertClass", func(t *testing.T) {
+				class := gocms.Class{
+					Slug: "insert_class",
+				}
+				assert.NoError(t, repo.InsertClass(&class))
+				assert.False(t, class.Id.IsZero())
+				assert.False(t, class.Created.IsZero())
+				assert.False(t, class.Updated.IsZero())
+			})
+
+			t.Run("UpdateClass", func(t *testing.T) {
+				class := gocms.Class{
+					Slug: "update_class",
+				}
+				assert.NoError(t, repo.InsertClass(&class))
+				updated := class.Updated
+
+				class.Slug += "_update"
+				assert.NoError(t, repo.UpdateClass(&class))
+				assert.True(t, updated.Before(class.Updated))
+
+				check, err := repo.GetClassById(class.Id)
+				assert.NoError(t, err)
+				assert.Equal(t, class.Slug, check.Slug)
+
+				class.Id = primitive.NewObjectID()
+				class.Slug = "update_class_fail"
+				assert.Error(t, repo.UpdateClass(&class))
+			})
+
+			t.Run("DeleteDocument", func(t *testing.T) {
+				doc := gocms.Document{}
+				assert.NoError(t, repo.InsertDocument(&doc))
+				assert.NoError(t, repo.DeleteDocument(doc.Id))
+
+				_, err := repo.GetDocumentById(doc.Id)
+				assert.Error(t, err)
+			})
+
+			t.Run("GetChildDocumentBySlug", func(t *testing.T) {
+				doc := gocms.Document{
+					ClassId:  primitive.NewObjectID(),
+					ParentId: primitive.NewObjectID(),
+					Slug:     "get_child_document_slug",
+				}
+				assert.NoError(t, repo.InsertDocument(&doc))
+
+				check, err := repo.GetChildDocumentBySlug(doc.ParentId, doc.Slug)
+				assert.NoError(t, err)
+				assert.False(t, doc.Id.IsZero())
+				assert.Equal(t, doc.Id, check.Id)
+
+				_, err = repo.GetChildDocumentBySlug(doc.ParentId, "invalid_slug")
+				assert.Error(t, err)
+			})
+
+			t.Run("GetClassDocumentBySlug", func(t *testing.T) {
+				doc := gocms.Document{
+					ClassId:  primitive.NewObjectID(),
+					ParentId: primitive.NewObjectID(),
+					Slug:     "get_class_document_slug",
+				}
+				assert.NoError(t, repo.InsertDocument(&doc))
+
+				check, err := repo.GetClassDocumentBySlug(doc.ClassId, doc.Slug)
+				assert.NoError(t, err)
+				assert.False(t, doc.Id.IsZero())
+				assert.Equal(t, doc.Id, check.Id)
+
+				_, err = repo.GetClassDocumentBySlug(doc.ClassId, "invalid_slug")
+				assert.Error(t, err)
+			})
+
+			t.Run("GetDocumentList", func(t *testing.T) {
+				classId := primitive.NewObjectID()
+				ids := make([]primitive.ObjectID, 3)
+
+				for i := range ids {
+					doc := gocms.Document{
+						ClassId: classId,
+						Slug:    fmt.Sprintf("test_%d", i),
+					}
+					assert.NoError(t, repo.InsertDocument(&doc))
+					ids[i] = doc.Id
+				}
+
+				params := gocms.DocumentListParams{
+					ClassId: classId,
+					Size:    2,
+					Page:    1,
+				}
+				page1, err := repo.GetDocumentList(params)
+				assert.NoError(t, err)
+				assert.Equal(t, 3, page1.Total)
+				assert.Equal(t, 2, len(page1.Documents))
+				for i := range ids[0:2] {
+					assert.Equal(t, ids[i], page1.Documents[i].Id)
+				}
+
+				params.Page = 2
+				page2, err := repo.GetDocumentList(params)
+				assert.NoError(t, err)
+				assert.Equal(t, 3, page2.Total)
+				assert.Equal(t, 1, len(page2.Documents))
+				for i := range ids[2:3] {
+					assert.Equal(t, ids[i], page1.Documents[i].Id)
+				}
+
+				params.ClassId = primitive.NewObjectID()
+				noResults, err := repo.GetDocumentList(params)
+				assert.NoError(t, err)
+				assert.Equal(t, 0, noResults.Total)
+			})
+
+			t.Run("GetDocumentById", func(t *testing.T) {
+				doc := gocms.Document{}
+				assert.NoError(t, repo.InsertDocument(&doc))
+
+				check, err := repo.GetDocumentById(doc.Id)
+				assert.NoError(t, err)
+				assert.False(t, doc.Id.IsZero())
+				assert.Equal(t, doc.Id, check.Id)
+			})
+
+			t.Run("InsertDocument", func(t *testing.T) {
+				doc := gocms.Document{
+					Slug: "create_document",
+				}
+				assert.NoError(t, repo.InsertDocument(&doc))
+				assert.False(t, doc.Id.IsZero())
+				assert.False(t, doc.Created.IsZero())
+				assert.False(t, doc.Updated.IsZero())
+			})
+
+			t.Run("UpdateDocument", func(t *testing.T) {
+				doc := gocms.Document{
+					ClassId: primitive.NewObjectID(),
+					Slug:    "update_document",
+				}
+				assert.NoError(t, repo.InsertDocument(&doc))
+
+				updated := doc.Updated
+				doc.Slug += "_update"
+				assert.NoError(t, repo.UpdateDocument(&doc))
+				assert.True(t, updated.Before(doc.Updated))
+
+				check, err := repo.GetDocumentById(doc.Id)
+				assert.NoError(t, err)
+				assert.Equal(t, doc.Slug, check.Slug)
+			})
+		})
+	}
+}
