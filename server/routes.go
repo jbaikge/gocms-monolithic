df9e0d71176c96191393b0e8d3d784cca4e8af41
jake <bigjake2s@gmail.com>
2022-04-28 09:06:56 -0400

    Login route and redirects to it

--- server/routes.go
+++ server/routes.go
@@ -1,19 +1,41 @@
 package server
 
 import (
+	"crypto/rand"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) Routes() *gin.Engine {
 	router := s.router
 
+	authKey := make([]byte, 64)
+	rand.Read(authKey)
+	store := cookie.NewStore(authKey)
+	router.Use(sessions.Sessions("gocms", store))
+
 	// router.GET("/assets/:filename", s.HandleAsset())
 	// router.GET("/forms/:id", s.HandleForm())
 	// router.POST("/forms/:id", s.HandleForm())
 
 	admin := router.Group("/admin")
+	admin.Use(s.MiddlewareAdminAuth())
 	admin.Use(s.MiddlewareNavBar())
 	{
+		admin.GET("/", func(c *gin.Context) {
+			c.Redirect(http.StatusPermanentRedirect, "/admin/login")
+		})
+
+		admin.GET("/login", s.HandleAdminLogin())
+		admin.POST("/login", s.HandleAdminLogin())
+
+		// Send successful logins here. Maybe just display a table with classes
+		// and document counts?
+		// admin.GET("/dashboard", s.HandleAdminDashboard())
+
 		classes := admin.Group("/classes")
 		{
 			classes.GET("/new", s.HandleClassBuilder())
